f25ca09153771a9bc7b729a8c73cdd2230499cc4
AndroidWenwen <87920151@qq.com>
2018-10-21 15:21:33 +0800

    Add files via upload

---
+++ pages/index/index.js
@@ -0,0 +1,26 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    studentList:[
+      {
+        name:'小明',
+        sex:'男',
+      },
+      {
+        name:'小红',
+        sex:'女'
+      },
+      {
+        name:'小华',
+        sex:'男'
+      }
+    ]
+  },
+  
+  onLoad: function () {
+   
+  },
+})
